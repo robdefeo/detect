4e6b4f3a73dc44e88fd2def0c0a0cf154486dd19
Rob De Feo <robertodefeo@hotmail.com>
2013-11-29 17:55:48 +0800

    do some logging right now!

--- app.js
+++ app.js
@@ -2,33 +2,74 @@ var
   express = require('express'),
   pos = require('pos'),
   preProcess = require('./lib/preProcess'),
-  disambiguator = require('./lib/entityDisambiguator');
-  var pjson = require('./package.json');
+  disambiguator = require('./lib/entityDisambiguator'),
+  pjson = require('./package.json'),
+  uuid = require('uuid'),
+  mongoClient = require('mongodb').MongoClient
 
 var app = express();
+var mongoUri = process.env.MONGOHQ_DETECTION_URL || "mongodb://localhost/detection" 
+
+var logResponse = function(detectionId, q, preProcessingResponse, disambiguatorResponse) {
+  mongoClient.connect(mongoUri, function (err, db) {
+    if (err) {
+      return console.dir(err);
+    }
+    var data = {
+      _id: detectionId,
+      q: q,
+      timestamp: new Date().toISOString(),
+      tokens: preProcessingResponse.tokens,
+      detections: disambiguatorResponse.detections,
+      nonDetections: disambiguatorResponse.nonDetections
+    };
+    db.collection("responses").insert(
+      data,
+      { 
+        safe: true
+      },
+      function (err, response){
+        if (err) {
+          return console.dir(err);
+        }
+    });
+  });
+};
 
 app.use(express.bodyParser());
 
+app.get('/status', function(req, res) {
+  res.json({
+    self: "up",
+    version: pjson.version
+  });
+});
+
 app.get('/', function(req, res) {
   var q = req.param('q');
+  var detectionId = uuid.v4();
   res.header("Access-Control-Allow-Origin", "*");
   // TODO: limit this based on environement or multiple but not *
   // res.header("Access-Control-Allow-Origin", "http://localhost:5000");
-  if (!q){
+  if (!q) {
     res.status(412).json({error: "Missing parameter: 'q'"});
   } else {
     console.log("received: " + q);
-    preProcess.do(q, function(preResult){
-      disambiguator.do(preResult, function(disambiguatorResult){
+    preProcess.do(q, function(preProcessingResponse){
+      disambiguator.do(preProcessingResponse, function(disambiguatorResponse){
         res.json({
-          // tokens: preResult.tokens,
+          detectionId: detectionId,
+          tokens: preProcessingResponse.tokens,
           version: pjson.version,
-          detections: disambiguatorResult.detections
-        });        
+          detections: disambiguatorResponse.detections,
+          nonDetections: disambiguatorResponse.nonDetections
+        }); 
+        
+        logResponse(detectionId, q, preProcessingResponse, disambiguatorResponse);       
       });
     });
   }
-})
+});
 
 
 var port = process.env.PORT || 5001;
