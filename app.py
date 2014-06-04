c94f4b8beb87a5598aa49f5e831addac2d95cadc
Rob De Feo <robertodefeo@hotmail.com>
2014-06-04 15:29:14 +0800

    handles exceptions closes #26

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import parse
 import vocab
-from flask import Flask, jsonify, request
+import traceback
+from flask import Flask, jsonify, request, abort
 from flask.ext.cors import cross_origin
 from bson.objectid import ObjectId
 
@@ -29,37 +30,47 @@ def vocab_load():
 @app.route('/', methods = ['GET'])
 @cross_origin()
 def api_root():
-  q = request.args.get("q")
-  sessionID = request.args.get("sessionID")
-  detectionID = ObjectId()
+  try:
+    q = request.args.get("q")
+    sessionID = request.args.get("sessionID")
+    detectionID = ObjectId()
 
-  print "app=detection,module=app,function=get,detectionId=%s,sessionID=%s,q=%s" % (detectionID, sessionID, q)
-  if not q or not sessionID:
+    print "app=detection,module=app,function=get,detectionId=%s,sessionID=%s,q=%s" % (detectionID, sessionID, q)
+    if not q or not sessionID:
+      resp = jsonify({
+        "status": "error",
+        "message": "missing param(s)"
+      })
+      resp.status_code = 412
+      return resp
+    else:
+      preprocessResult = parse.preperation(q)
+      disambiguateResult = parse.disambiguate(data, preprocessResult)
+      print disambiguateResult
+      log = {
+        "detectionId": str(detectionID),
+        "tokens": preprocessResult["tokens"],
+        "detections": disambiguateResult["detections"],
+        "nonDetections": disambiguateResult["nonDetections"]
+      }
+      res = {
+        "detectionId": str(detectionID),
+        # version: pjson.version,
+        "detections": disambiguateResult["detections"],
+        "nonDetections": disambiguateResult["nonDetections"]
+      }
+      resp = jsonify(res)
+      resp.status_code = 200
+      return resp
+  except Exception,e:
+    print "error=%s" % (traceback.format_exc())
     resp = jsonify({
       "status": "error",
-      "message": "missing param(s)"
+      "exception": traceback.format_exc()
     })
-    resp.status_code = 412
+    resp.status_code = 500
     return resp
-  else:
-    preprocessResult = parse.preperation(q)
-    disambiguateResult = parse.disambiguate(data, preprocessResult)
-    print disambiguateResult
-    log = {
-      "detectionId": str(detectionID),
-      "tokens": preprocessResult["tokens"],
-      "detections": disambiguateResult["detections"],
-      "nonDetections": disambiguateResult["nonDetections"]
-    }
-    res = {
-      "detectionId": str(detectionID),
-      # version: pjson.version,
-      "detections": disambiguateResult["detections"],
-      "nonDetections": disambiguateResult["nonDetections"]
-    }
-    resp = jsonify(res)
-    resp.status_code = 200
-    return resp
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
